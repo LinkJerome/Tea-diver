e69f897f170a040a3671b674b98a148e4929d965
VIGLIANO LEA p1517348 <lea.vigliano@etu.univ-lyon1.fr>
2022-01-16 00:47:20 +0100

    Ajout base de l'API

---
+++ api/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const routes = require("./routes/tea");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const cors = require("cors");
+const compression = require("compression");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(helmet());
+app.use(compression()); // Compress all routes
+
+app.use("/public", express.static(process.cwd() + "/public")); // make public static
+app.use("/uploads", express.static("./uploads")); // makes uploads folder available
+
+app.use(cors({ origin: "https://victoria-lo.github.io/Hashtag-TEA/" }));
+
+app.use(express.json()); // parses incoming requests as JSON
+app.use("/", routes);
+
+// Index page (static HTML)
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/index.html");
+});
+
+// establish connection to database
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    auth: { authSource: "admin" },
+    user: process.env.MONGO_ROOT_USER,
+    pass: process.env.MONGO_ROOT_PASSWORD,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  },
+  function (err) {
+    if (err) return console.log("Error: ", err);
+    console.log(
+      "MongoDB Connection -- Ready state is:",
+      mongoose.connection.readyState
+    );
+  }
+);
+
+app.listen(process.env.PORT || 3000);
