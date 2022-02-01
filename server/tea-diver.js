fbd7743999e35c9abc5a7ec65d6ee92a987bfef1
P1507711 <guillaume.treme@etu.univ-lyon1.fr>
2022-02-01 17:49:41 +0100

    back thermometer

--- server/tea-diver.js
+++ server/tea-diver.js
@@ -1,4 +1,4 @@
-const { Board, Led } = require('johnny-five');
+const { Board, Led, Thermometer } = require('johnny-five');
 const board = new Board({ port: 'COM6' });
 
 board.on('ready', () => {
@@ -7,9 +7,20 @@ board.on('ready', () => {
   const led = new Led(13);
   led.blink(500);
   // Test Thermometer - DS18B20
+  const thermometer = new Thermometer({
+    controller: 'DS18B20',
+    pin: 2, // Modify if needed
+  });
 
   board.loop(2000, () => {
     // Test Thermometre
+    thermometer.on('change', () => {
+      const { celsius, fahrenheit, kelvin } = thermometer;
+      console.log('  celsius      : ', celsius);
+      console.log('  fahrenheit   : ', fahrenheit);
+      console.log('  kelvin       : ', kelvin);
+      console.log('--------------------------------------');
+    });
   });
 
   board.on('exit', () => {
