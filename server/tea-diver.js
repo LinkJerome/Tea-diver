c224fda9073b11e8646e65baa38ef7c516cad85c
P1507711 <guillaume.treme@etu.univ-lyon1.fr>
2022-02-01 10:25:08 +0100

    save

--- server/tea-diver.js
+++ server/tea-diver.js
@@ -1,71 +1,70 @@
-const five = require("johnny-five");
+// const five = require('johnny-five');
 
-
-const { Board, Led, Servo } = require('johnny-five');
-const Thermometer = five.Thermometer;
-const board = new Board({ port: "COM6" });
+const { Board, Led } = require('johnny-five');
+// const Thermometer = five.Thermometer;
+const board = new Board({ port: '/dev/ttyUSB4' });
 
 board.on('ready', () => {
-    console.log('Arduino Connected');
-    // Test LED
-    const led = new Led(13);
-    led.blink(500);
-    // Test Thermometer - DS18B20
-    const thermometer = new Thermometer({
-        controller: 'DS18B20',
-        pin: 2,
-    });
+  console.log('Arduino Connected');
+  // Test LED
+  const led = new Led(13);
+  led.blink(500);
+  // Test Thermometer - DS18B20
+  // const thermometer = new Thermometer({
+  //  controller: 'DS18B20',
+  //  pin: 2,
+  // });
 
-    board.loop(500, () => {
-        // Test Thermometre
-        thermometer.on('change', () => {
-            const { address, celsius, fahrenheit, kelvin } = thermometer;
-            console.log(`Thermometer at address: 0x${address.toString(16)}`);
-            console.log('  celsius      : ', celsius);
-            console.log('  fahrenheit   : ', fahrenheit);
-            console.log('  kelvin       : ', kelvin);
-            console.log('--------------------------------------');
-        });
-    });
+  board.loop(500, () => {
+    // Test Thermometre
+    // thermometer.on('change', () => {
+    //  const { address, celsius, fahrenheit, kelvin } = thermometer;
+    //  console.log(`Thermometer at address: 0x${address.toString(16)}`);
+    //  console.log('  celsius      : ', celsius);
+    //  console.log('  fahrenheit   : ', fahrenheit);
+    //  console.log('  kelvin       : ', kelvin);
+    //  console.log('--------------------------------------');
+    // });
+  });
 
-    // Test Servo
-    // const servo = new Servo(10);
+  // Test Servo
+  // const servo = new Servo(10);
 
-    // Servo API
+  // Servo API
 
-    // min()
-    //
-    // set the servo to the minimum degrees
-    // defaults to 0
-    //
-    // eg. servo.min();
+  // min()
+  //
+  // set the servo to the minimum degrees
+  // defaults to 0
+  //
+  // eg. servo.min();
 
-    // max()
-    //
-    // set the servo to the maximum degrees
-    // defaults to 180
-    //
-    // eg. servo.max();
+  // max()
+  //
+  // set the servo to the maximum degrees
+  // defaults to 180
+  //
+  // eg. servo.max();
 
-    // center()
-    //
-    // centers the servo to 90°
-    //
-    // servo.center();
+  // center()
+  //
+  // centers the servo to 90°
+  //
+  // servo.center();
 
-    // to( deg )
-    //
-    // Moves the servo to position by degrees
-    //
-    // servo.to( 90 );
+  // to( deg )
+  //
+  // Moves the servo to position by degrees
+  //
+  // servo.to( 90 );
 
-    // step( deg )
-    //
-    // step all servos by deg
-    //
-    // eg. array.step( -20 );
+  // step( deg )
+  //
+  // step all servos by deg
+  //
+  // eg. array.step( -20 );
 
-    board.on('exit', () => {
-        console.log('Disconnection of Arduino');
-    });
+  board.on('exit', () => {
+    console.log('Disconnection of Arduino');
+  });
 });
